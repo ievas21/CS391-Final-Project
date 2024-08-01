96df4d34075ddcc2154abf93293f86fc96ce8cba
Ieva <ievas@bu.edu>
2024-08-01 18:43:02 -0400

    Updated the styling for the login component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import {createGlobalStyle} from "styled-components";
 const GlobalStyle = createGlobalStyle`
     html {
         margin: 0;
-        box-sizing: border-box; }
+        box-sizing: border-box;
+        background-color: #F3EBF6;
+        font-family: 'Ubuntu', sans-serif;
+    }
 `;
 
 function App() {
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,60 @@
 import styled from "styled-components";
 
 const StyledDiv = styled.div`
+    background-color: #FFFFFF;
+    width: 400px;
+    height: 400px;
+    margin: 7em auto;
+    border-radius: 1.5em;
+    box-shadow: 0 11px 35px 2px rgba(0, 0, 0, 0.14);
+`;
+
+const StyledP = styled.p`
+    padding-top: 40px;
+    color: rgb(104, 119, 244);
+    font-family: Georgia, serif;
+    font-weight: bold;
+    font-size: 35px;
+    margin-bottom: 18px;
     display: flex;
     justify-content: center;
-    align-items: center;
-    text-align: center;
-    min-height: 100vh;
-
 `;
 
 const StyledInput = styled.input`
-    padding: 3px;
-    margin: 0 auto;
-
+    width: 82%;
+    color: rgb(38, 50, 56);
+    font-weight: 700;
+    font-size: 14px;
+    letter-spacing: 1px;
+    background: rgba(136, 126, 126, 0.04);
+    padding: 15px 20px;
+    border-radius: 20px;
+    border: 2px solid #808080;
+    margin: 25px 15px;
+    text-align: center;
+    display: flex;
+    justify-content: center;
 `;
 
-const StyledLabel = styled.label`
-    display: block;
-
+const StyledForm = styled.form`
+    padding-top: 10px;
+    padding-bottom: 18px;
 `;
+
+const StyledButton = styled.button`
+    cursor: pointer;
+    border-radius: 5em;
+    color: #fff;
+    font-family: "Monaco", "Courier New", serif;
+    font-weight: bold;
+    background: linear-gradient(110.6deg, rgb(184, 142, 252) 2.2%, rgb(104, 119, 244) 100.2%);
+    border: 0;
+    padding: 10px 40px;
+    margin-left: 35%;
+    font-size: 15px;
+    box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.04);
+`;
+
 
 export default function Login() {
 
@@ -29,19 +65,14 @@ export default function Login() {
     return(
         <>
             <StyledDiv>
-                <StyledLabel>
-                    Username: <StyledInput name="username" type="text"/>
-                </StyledLabel>
+                <StyledP>Sign-In</StyledP>
+                <StyledForm>
+                    <StyledInput name="username" type="text" placeholder="Username"/>
 
-                <br/><br/>
+                    <StyledInput name="password" type="password" placeholder="Password"/>
+                </StyledForm>
 
-                <StyledLabel>
-                    Password: <StyledInput name="password" type="password"/>
-                </StyledLabel>
-
-                <br/><br/><br/>
-
-                <button onClick={handleSubmit}>Login</button>
+                <StyledButton onClick={handleSubmit}>Login</StyledButton>
             </StyledDiv>
         </>
     )
